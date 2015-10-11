de8d687649d524cef8281d342581c826c66166d2
Marcel Groeneweg <marcel.groeneweg@itvisors.com>
2015-10-11 14:36:32 +0200

    Initial commit
    Initial commit

---
+++ GeoLocationWidget/src/GeoLocationWidget/widget/GeoLocationWidget.js
@@ -0,0 +1,202 @@
+/*jshint undef: true, browser:true, nomen: true */
+/*jslint browser:true, nomen: true */
+/*global mx, console, define, require */
+/*
+    GeoLocationWidget
+    ========================
+
+    @file      : GeoLocationWidget.js
+    @version   : 1.0
+    @author    : Marcel Groeneweg
+    @date      : Sun, 11 Oct 2015 07:05:38 GMT
+    @copyright : 
+    @license   : Apache 2
+
+    Documentation
+    ========================
+    Use Google client library to get current location. The Google client library takes WiFi access points into account to determine the location.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/dom-geometry",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event",
+    "big/big",
+    "GeoLocationWidget/lib/jquery-1.11.2",
+    "dojo/text!GeoLocationWidget/widget/template/GeoLocationWidget.html"
+], function (declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, Big, _jQuery, widgetTemplate) {
+    "use strict";
+
+    var $ = _jQuery.noConflict(true);
+    
+    // Declare widget's prototype.
+    return declare("GeoLocationWidget.widget.GeoLocationWidget", [ _WidgetBase ], {
+
+        // DOM elements
+
+        // Parameters configured in the Modeler.
+        buttonLabel: "",
+        buttonClass: "",
+        latAttr: "",
+        longAttr: "",
+        onChangeMF: "",
+
+        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+        _handles: null,
+        _contextObj: null,
+        _result : null,
+        _button : null,
+        _hasStarted : false,
+
+        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+        constructor: function () {
+            this._handles = [];
+        },
+
+        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+        postCreate: function () {
+            console.log(this.id + ".postCreate");
+            
+            // Placeholder container
+            this._button = dom.div();
+            dojoClass.add(this._button, "btn");
+            if (this.buttonClass) {
+                dojoClass.add(this._button, this.buttonClass);
+            } else {
+                dojoClass.add(this._button, "btn-primary");
+            }
+
+            dojoHtml.set(this._button, this.buttonLabel || "GEO Location");
+
+            // Add to widget node
+            this.domNode.appendChild(this._button);
+            
+            this._updateRendering();
+            this._setupEvents();
+        },
+
+        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+        update: function (obj, callback) {
+            console.log(this.id + ".update");
+
+            this._contextObj = obj;
+            this._resetSubscriptions();
+            this._updateRendering();
+
+            callback();
+        },
+
+        // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
+        enable: function () {},
+
+        // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
+        disable: function () {},
+
+        // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
+        resize: function (box) {},
+
+        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+        uninitialize: function () {
+            // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+        },
+
+        // We want to stop events on a mobile device
+        _stopBubblingEventOnMobile: function (e) {
+            if (typeof document.ontouchstart !== "undefined") {
+                dojoEvent.stop(e);
+            }
+        },
+
+        // Attach events to HTML dom elements
+        _setupEvents: function () {
+            this.connect(this._button, "click", dojoLang.hitch(this, function (evt) {
+                console.log("GEO Location start getting location.");
+
+                // The camera function has a success, failure and a reference to this.
+                navigator.geolocation.getCurrentPosition(
+                    dojoLang.hitch(this, this._geolocationSuccess),
+                    dojoLang.hitch(this, this._geolocationFailure),
+                    {timeout: 10000}
+                );
+            }));
+        },
+
+        // Rerender the interface.
+        _updateRendering: function () {
+        },
+
+
+        _geolocationSuccess: function (position) {
+
+            this._contextObj.set(this.latAttr, new Big(position.coords.latitude).round(8));
+            this._contextObj.set(this.longAttr, new Big(position.coords.longitude).round(8));
+            this._executeMicroflow();
+        },
+
+        _geolocationFailure: function (error) {
+
+            console.log("GEO Location failure!");
+            console.log(error.message);
+
+            if (this._result) {
+                dojoHtml.set(this._result, "GEO Location failure...");
+            } else {
+                this._result = dom.div();
+                dojoHtml.set(this._result, "GEO Location failure...");
+                this.domNode.appendChild(this._result);
+            }
+        },
+
+        _executeMicroflow: function () {
+
+            if (this.onChangeMF && this._contextObj) {
+                mx.processor.xasAction({
+                    error       : function () {},
+                    actionname  : this.onChangeMF,
+                    applyto     : "selection",
+                    guids       : [this._contextObj.getGuid()]
+                });
+            }
+        },
+
+        // Reset subscriptions.
+        _resetSubscriptions: function () {
+            // Release handles on previous object, if any.
+            if (this._handles) {
+                this._handles.forEach(function (handle) {
+                    mx.data.unsubscribe(handle);
+                });
+                this._handles = [];
+            }
+
+            // When a mendix object exists create subscribtions. 
+            if (this._contextObj) {
+                var objectHandle = this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    callback: dojoLang.hitch(this, function (guid) {
+                        this._updateRendering();
+                    })
+                });
+
+
+                this._handles = [ objectHandle ];
+            }
+        }
+    });
+});
+
+require(["GeoLocationWidget/widget/GeoLocationWidget"], function () {
+    "use strict";
+});
