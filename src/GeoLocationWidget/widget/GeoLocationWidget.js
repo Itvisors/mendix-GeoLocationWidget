bc86d4bf659eab27495a4619163dadf6bd8d2a32
Marcel Groeneweg <marcel.groeneweg@itvisors.com>
2015-10-19 17:33:12 +0200

    Show map
    Show map with draggable marker.

--- src/GeoLocationWidget/widget/GeoLocationWidget.js
+++ src/GeoLocationWidget/widget/GeoLocationWidget.js
@@ -15,26 +15,20 @@
     Documentation
     ========================
     Use Google client library to get current location. The Google client library takes WiFi access points into account to determine the location.
+    This widget depends on the original GoogleMaps widget for the AMD loader. 
 */
 
 // Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
 define([
     "dojo/_base/declare",
     "mxui/widget/_WidgetBase",
-    "mxui/dom",
-    "dojo/dom",
-    "dojo/dom-prop",
-    "dojo/dom-geometry",
     "dojo/dom-class",
     "dojo/dom-style",
-    "dojo/dom-construct",
-    "dojo/_base/array",
     "dojo/_base/lang",
-    "dojo/text",
     "dojo/html",
     "dojo/_base/event",
-    "dojo/request/script"
-], function (declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, dojoScript) {
+    "GoogleMaps/lib/googlemaps!"
+], function (declare, _WidgetBase, dojoClass, dojoStyle, dojoLang, dojoHtml, dojoEvent, googleMaps) {
     "use strict";
 
     // Declare widget's prototype.
@@ -54,9 +48,11 @@ define([
         // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
         _handles: null,
         _contextObj: null,
-        _result : null,
-        _button : null,
-        _hasStarted : false,
+        _result: null,
+        _button: null,
+        _mapContainer: null,
+        _googleMap: null,
+        _marker: null,
 
         // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
         constructor: function () {
@@ -68,6 +64,19 @@ define([
             //console.log(this.id + ".postCreate");
             
             // Placeholder container
+            this._mapContainer = document.createElement("div");
+            if (this.mapClass) {
+                dojoClass.add(this._mapContainer, this.mapClass);
+            } else {
+                dojoStyle.set(this._mapContainer, {
+                    height: "350px",
+                    width: "100%"
+                });
+            }
+            // Add to widget node
+            this.domNode.appendChild(this._mapContainer);
+
+            // Button
             this._button = document.createElement("div");
             dojoClass.add(this._button, "btn");
             if (this.buttonClass) {
@@ -83,6 +92,14 @@ define([
 
             this._updateRendering();
             this._setupEvents();
+
+            if (google.maps) {
+                this._loadMap();
+            } else {
+                window[this.id + "_mapsCallback"] = dojoLang.hitch(this, function () {
+                    this._loadMap();
+                });
+            }
         },
 
         // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
@@ -103,11 +120,19 @@ define([
         disable: function () {},
 
         // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
-        resize: function (box) {},
+        resize: function (box) {
+            if (this._googleMap) {
+                google.maps.event.trigger(this._googleMap, "resize");
+            }
+        },
 
         // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
         uninitialize: function () {
             // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+            this._marker.setMap(null);
+            this._marker = null;
+            
+            window[this.id + "_mapsCallback"] = null;
         },
 
         // We want to stop events on a mobile device
@@ -124,7 +149,7 @@ define([
 
                 // The camera function has a success, failure and a reference to this.
                 navigator.geolocation.getCurrentPosition(
-                    dojoLang.hitch(this, this._geolocationSuccess),
+                    dojoLang.hitch(this, this._geolocationSucces),
                     dojoLang.hitch(this, this._geolocationFailure),
                     {timeout: 10000, enableHighAccuracy: true}
                 );
@@ -133,24 +158,56 @@ define([
 
         // Rerender the interface.
         _updateRendering: function () {
+            if (this._googleMap) {
+                this._showMarker();
+                google.maps.event.trigger(this._googleMap, "resize");
+            }
         },
 
+        _geolocationSucces: function (position) {
+            this._setCurrentLocation(position.coords.latitude, position.coords.longitude);
+        },
 
-        _geolocationSuccess: function (position) {
+        _setCurrentLocation: function (latitude, longitude) {
 
             var geocoder,
                 latlng;
             
-            this._contextObj.set(this.latAttr, position.coords.latitude.toFixed(8));
-            this._contextObj.set(this.longAttr, position.coords.longitude.toFixed(8));
+            this._contextObj.set(this.latAttr, latitude.toFixed(8));
+            this._contextObj.set(this.longAttr, longitude.toFixed(8));
+            this._showMarker();
             if (this.reverseGeoCodingAction === "locationOnly") {
                 this._executeMicroflow();
             } else {
                 geocoder = new google.maps.Geocoder();
-                latlng = {lat: position.coords.latitude, lng: position.coords.longitude};
+                latlng = {lat: latitude, lng: longitude};
                 geocoder.geocode({
                     "location": latlng
                 }, dojoLang.hitch(this, this._reverseGeoCodingSuccess));
+            }
+        },
+        
+        _showMarker: function () {
+            var latLng;
+            
+            if (this._marker === null) {
+                this._marker = new google.maps.Marker({
+                    map: this._googleMap,
+                    draggable: true
+                });
+                // Register Custom "dragend" Event
+                google.maps.event.addListener(this._marker, "dragend", dojoLang.hitch(this, function () {
+                    // Get the position where the pointer was dropped
+                    var point = this._marker.getPosition();
+                    // Move map to marker location
+                    this._setCurrentLocation(point.lat(), point.lng());
+                }));
+            }
+            latLng = this._createLatLng();
+            this._marker.setPosition(latLng);
+            this._googleMap.panTo(latLng);
+            if (this.markerDisplayAttr) {
+                this._marker.setTitle(this._contextObj.get(this.markerDisplayAttr));
             }
         },
         
@@ -159,7 +216,7 @@ define([
             
             if (status === google.maps.GeocoderStatus.OK) {
                 console.log("Reverse geocode OK");
-                console.dir(results);
+                //console.dir(results);
                 if (this.reverseGeoCodingAction === "fullResult") {
                     resultString = JSON.stringify(results);
                     this._contextObj.set(this.reverseGeocodingResultJson, resultString);
@@ -186,6 +243,37 @@ define([
                 dojoHtml.set(this._result, "GEO Location failure...");
                 this.domNode.appendChild(this._result);
             }
+        },
+
+        _loadMap: function () {
+                        
+            this._googleMap = new google.maps.Map(this._mapContainer, {
+                zoom: this.defaultZoom,
+                center: new google.maps.LatLng(this.defaultLat, this.defaultLng),
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                mapTypeControlOption: {
+                    style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR
+                }
+            });
+
+        },
+        
+        _createLatLng: function () {
+            var lat,
+                lng;
+
+            if (this._contextObj) {
+                lat = this._contextObj.get(this.latAttr);
+                lng = this._contextObj.get(this.longAttr);
+            }
+            if (lat === null || lat === undefined) {
+                lat = 0;
+            }
+            if (lng === null || lng === undefined) {
+                lng = 0;
+            }
+            
+            return new google.maps.LatLng(lat, lng);
         },
 
         _executeMicroflow: function () {
